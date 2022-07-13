09af27d82e2173c8fe474e98bb3d3a79212752a9
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-13 20:54:05 -0300

    display teams by conference and division

--- Pages/Teams/Index.cshtml.cs
+++ Pages/Teams/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using League.Models;
 using League.Data;
+using System.Linq;
 
 
 namespace League.Pages.Teams
@@ -19,10 +20,26 @@ namespace League.Pages.Teams
             _context = context;
         }
 
+        public List<Conference> Conferences {get; set;}
+        public List<Division> Divisions {get; set;}
         public List<Team> Teams {get; set;}
         public async Task OnGetAsync()
         {
+            Conferences = await _context.Conferences.ToListAsync();
+            Divisions = await _context.Divisions.ToListAsync();
             Teams = await _context.Teams.ToListAsync();
+
+
+        }
+
+        public List<Division> GetConferenceDivisions(string ConferenceId)
+        {
+            return Divisions.Where(d => d.ConferenceId.Equals(ConferenceId)).OrderBy(d => d.Name).ToList();
+        }
+
+        public List<Team> GetDivisionTeams(string DivisionId)
+        {
+            return Teams.Where(t => t.DivisionId.Equals(DivisionId)).OrderBy(t => t.Name).ToList();
         }
     }
 }
