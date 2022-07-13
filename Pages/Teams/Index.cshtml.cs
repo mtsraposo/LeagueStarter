fa537ff1f2ef70b655ce09feae0fba98b9fa0584
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-13 20:38:53 -0300

    add minimal teams index page

---
+++ Pages/Teams/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using League.Models;
+using League.Data;
+
+
+namespace League.Pages.Teams
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LeagueContext _context;
+
+        public IndexModel(LeagueContext context)
+        {
+            _context = context;
+        }
+
+        public List<Team> Teams {get; set;}
+        public async Task OnGetAsync()
+        {
+            Teams = await _context.Teams.ToListAsync();
+        }
+    }
+}
