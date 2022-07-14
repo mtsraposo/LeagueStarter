04ef4f17ed78329696092de7f4dd63a708be7037
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-13 21:14:37 -0300

    add favorite selection to header

--- Pages/Teams/Index.cshtml.cs
+++ Pages/Teams/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 using League.Models;
 using League.Data;
 using System.Linq;
@@ -14,7 +15,6 @@ namespace League.Pages.Teams
     public class IndexModel : PageModel
     {
         private readonly LeagueContext _context;
-
         public IndexModel(LeagueContext context)
         {
             _context = context;
@@ -23,13 +23,29 @@ namespace League.Pages.Teams
         public List<Conference> Conferences {get; set;}
         public List<Division> Divisions {get; set;}
         public List<Team> Teams {get; set;}
+        [BindProperty(SupportsGet = true)]
+        public string FavoriteTeam {get; set;}
+        public SelectList AllTeams {get; set;}
         public async Task OnGetAsync()
         {
             Conferences = await _context.Conferences.ToListAsync();
             Divisions = await _context.Divisions.ToListAsync();
             Teams = await _context.Teams.ToListAsync();
 
+            IQueryable<string> teamQuery = from t in _context.Teams
+                                            orderby t.TeamId
+                                            select t.TeamId;
 
+            AllTeams = new SelectList(await teamQuery.ToListAsync());
+
+            if (FavoriteTeam != null)
+            {
+                HttpContext.Session.SetString("_Favorite", FavoriteTeam);
+            }
+            else
+            {
+                FavoriteTeam = HttpContext.Session.GetString("_Favorite");
+            }
         }
 
         public List<Division> GetConferenceDivisions(string ConferenceId)
@@ -39,7 +55,8 @@ namespace League.Pages.Teams
 
         public List<Team> GetDivisionTeams(string DivisionId)
         {
-            return Teams.Where(t => t.DivisionId.Equals(DivisionId)).OrderBy(t => t.Name).ToList();
+            return Teams.Where(t => t.DivisionId.Equals(DivisionId)).OrderBy(t => t.Win).ToList();
         }
+
     }
 }
